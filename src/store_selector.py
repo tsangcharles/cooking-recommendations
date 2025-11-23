4716c50281462b7a906c0e9b92ad5c60c6d65749
Charles <ck2tsang@gmail.com>
2025-11-22 20:16:28 -0500

    fix: Fix Docker build for Raspberry Pi by switching to Chromium

--- src/store_selector.py
+++ src/store_selector.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 from datetime import datetime
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 
 class FlippStoreSelector:
@@ -13,8 +14,9 @@ class FlippStoreSelector:
         self.driver = None
         
     def setup_driver(self):
-        """Initialize Chrome driver with options"""
+        """Initialize Chromium driver with options"""
         chrome_options = Options()
+        chrome_options.binary_location = "/usr/bin/chromium"
         if self.headless:
             chrome_options.add_argument('--headless=new')
         chrome_options.add_argument('--no-sandbox')
@@ -24,7 +26,8 @@ class FlippStoreSelector:
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_argument('--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
         
-        self.driver = webdriver.Chrome(options=chrome_options)
+        service = Service('/usr/bin/chromedriver')
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
         self.driver.implicitly_wait(10)
         
     def select_store(self, postal_code="L6E1T8"):
